29121535579145c0a835d7bb7e685b6ac6c311b6
farhannellipparamban <anufarhan111@gmail.com>
2023-08-03 21:23:07 +0530

    fornt end updation

--- config/config.js
+++ config/config.js
@@ -1,15 +1,16 @@
 const sessionSecret = "mysitesessionsecret";
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const mongooseConnect = ()=>{
-  mongoose.connect("mongodb://127.0.0.1:27017/watch_palace")
-  .then(()=>{
-    console.log("database Connected");
-  }).catch((error)=>{
-    console.log(error);
-  })
-}
-
+const mongooseConnect = () => {
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/watch_palace")
+    .then(() => {
+      console.log("database Connected");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 module.exports = {
   sessionSecret,
