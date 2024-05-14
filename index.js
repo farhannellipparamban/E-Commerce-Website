5826fca3424a914917f0c59feb20888db181d4c2
farhannellipparamban <anufarhan111@gmail.com>
2024-05-14 12:35:56 +0530

    port updated

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use("/", userRoute);
 const adminRoute = require("./routes/adminRoute");
 app.use("/admin", adminRoute);
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000; 
 
 app.listen(port, () => {
   console.log("server is running....http://localhost:3000");
